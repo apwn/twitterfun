952bc729e850df86d08a34728ed74052183c7edc
apwn <thomas.kanze@gmail.com>
2016-05-26 11:22:13 +1000

    new functionnalities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'bcrypt'
+
+@password = BCrypt::Password.create("password123", :cost => 13)
+
+users = User.create([{name: 'Thomas', email: 'thomas@example.com', password_digest: @password},
+  {name: 'Brad', email: 'brad@example.com', password_digest: @password},
+  {name: 'Luke', email: 'luke@example.com', password_digest: @password},
+  {name: 'Will', email: 'will@example.com', password_digest: @password},
+  {name: 'Jason', email: 'jason@example.com', password_digest: @password},
+  {name: 'Stephen', email: 'stephen@example.com', password_digest: @password},
+  {name: 'Niall', email: 'niall@example.com', password_digest: @password},
+  {name: 'Ephrem', email: 'ephrem@example.com', password_digest: @password},
+  {name: 'Yang', email: 'yang@example.com', password_digest: @password},
+  {name: 'Yogi', email: 'yogi@example.com', password_digest: @password}])
+
+#Post.create(content: 'something; cos I embody coolness', user: users[3])
